985f2445a3062bd1b8915483b45a860590d4c9f7
redbearin <perrigoj@bellsouth.net>
2019-03-22 10:55:00 -0700

    cache invalidation weather and meetups

--- data/schema.sql
+++ data/schema.sql
@@ -6,8 +6,9 @@ CREATE TABLE locations (
   id SERIAL PRIMARY KEY,
   search_query VARCHAR(255),
   formatted_query VARCHAR(255),
-  latitude NUMERIC(8, 6),
-  longitude NUMERIC(9,6)
+  latitude NUMERIC(10, 7),
+  longitude NUMERIC(10,7),
+  created_at BIGINT
 );
 
 CREATE TABLE weathers (
@@ -25,6 +26,7 @@ CREATE TABLE meetups (
   name VARCHAR(255),
   creation_date VARCHAR(255),
   host VARCHAR(255),
+  created_at BIGINT,
   location_id INTEGER NOT NULL,
   FOREIGN KEY (location_id) REFERENCES locations (id)
 );
